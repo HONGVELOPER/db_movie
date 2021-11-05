ebf601739c3401edaf518cc6d5b5461b010c13c0
Kimhannah <skqkdldhf98@naver.com>
2021-11-06 00:26:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Movie from "./pages/hong/Movie";
 import Seat from "./pages/hong/Seat";
 import CommutePage from "./pages/choi/CommutePage";
 import SalaryPage from "./pages/choi/SalaryPage";
+import LoginPage from "./pages/kim/LoginPage";
 
 function App() {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/movie" component={Movie} />
-      <Route path="/movie/seat" component={Seat} />
+    return (
+        <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/movie" component={Movie} />
+            <Route path="/movie/seat" component={Seat} />
 
-      <Route path="/commute" component={CommutePage} />
-      <Route path="/salary" component={SalaryPage} />
-    </div>
-  );
+            <Route path="/commute" component={CommutePage} />
+            <Route path="/salary" component={SalaryPage} />
+
+            <Route path="/login" component={LoginPage} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/kim/LoginPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1> Login Page </h1>
+        </div>
+    );
+};
+
+export default LoginPage;
