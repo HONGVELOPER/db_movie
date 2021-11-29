b337370b30734af2f3ef7e7979ca2326abf2ba57
Young-Jin1003 <hyjinn1003@naver.com>
2021-11-29 17:06:18 +0900

    비회원 예매 block, 결제 버튼 클릭시 로직 처리 후 payment/payment 로 redirect 처리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import LogInOut from "../components/kim/LogInOut";
+import { useRecoilState } from "recoil";
+import { loginState } from "../loginState";
+import { useHistory } from "react-router";
  
 const Home = () => {
+    const [isLogin, setIsLogin] = useRecoilState(loginState);
+    const history = useHistory();
+
+    const toMovie = () => {
+        if (!isLogin) {
+            alert("로그인이 필요합니다.")
+        } else {
+            history.push({
+                pathname: '/movie',
+                state: {
+                    email: isLogin.email
+                }
+            })
+        }
+    }
+
     return (
         <>
             <h1>Home page</h1>
-            <Button type="button">
-                <Link to="/movie">
-                    예매
-                </Link>
+            <Button type="button" onClick={toMovie}>
+                {/* <Link to="/movie">
+                </Link> */}
+                예매
             </Button>
             <LogInOut />
         </>
