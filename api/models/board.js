7c4b172c56e5f25ca8b23f4dcef65c194688c9c4
Kimhannah <skqkdldhf98@naver.com>
2021-12-03 15:07:04 +0900

    테이블명 BOARD -> B_BOARD로 수정

--- api/models/board.js
+++ api/models/board.js
@@ -1,36 +1,40 @@
-module.exports = function(sequelize, DataTypes) {
-    const board = sequelize.define('board', {
-        ID: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
+module.exports = function (sequelize, DataTypes) {
+    const board = sequelize.define(
+        "B_BOARD",
+        {
+            ID: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            Writer: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            Title: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            Content: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            CreatedAt: {
+                type: DataTypes.DATE,
+                defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+                allowNull: false,
+            },
         },
-        Writer: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        Title: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        Content: { 
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        CreatedAt: {
-            type: DataTypes.DATE,
-            defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-            allowNull: false,
+        {
+            charset: "utf8",
+            collate: "utf8_unicode_ci",
+            dateStrings: true,
+            typeCast: true,
+            freezeTableName: true,
+            tableName: "Board",
+            timestamps: false,
         }
-    }, {
-        charset: 'utf8',
-        collate: 'utf8_unicode_ci',
-        dateStrings: true,
-        typeCast: true,
-        freezeTableName: true,
-        tableName: 'Board',
-        timestamps: false,
-    })
-    return board
-}
+    );
+    return board;
+};
